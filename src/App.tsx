1de90f3a968aa0bda5d15b75c3f3923c251c6d52
mkfuget <michaelfuget@gmail.com>
2021-07-25 23:01:41 -0700

    adding sidebar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import {BogglePuzzle} from './modules/boggle/components/BogglePuzzle'
+import {Module} from './modules/Module'
 import {store} from "./store"
 import {Provider} from "react-redux"
 function App() {
   return (
     <Provider store = {store}>
       <div className="App">
-          <BogglePuzzle />
+          <Module/>
       </div>
     </Provider>
   );
---
+++ src/modules/Module.tsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import {Button} from 'react-bootstrap'
+import { BogglePuzzle } from './boggle/components/BogglePuzzle';
+import ConceptsSideBar from './conceptsSideBar/ConceptsSideBar';
+type BoardProps = {
+    board: string[][];
+    dictionary: string[];
+}
+
+  
+export const Module = () => {
+    return (
+        <>
+            <BogglePuzzle/>
+            <ConceptsSideBar/>
+        </>
+    )
+};
