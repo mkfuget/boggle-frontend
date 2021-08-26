58e27bc6e34ed77da61e0f86858dab7bfe74d0de
mkfuget <michaelfuget@gmail.com>
2021-08-25 20:15:51 -0700

    adding AWS Cognito

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_4OaklmNuP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4iuuna9s4qnv3ajbc8k4p09gnt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
