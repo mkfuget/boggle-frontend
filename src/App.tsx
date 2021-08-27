d3895602cac1bfcc4a4db8d22eb3937b5d2063e4
mkfuget <michaelfuget@gmail.com>
2021-08-27 15:29:34 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {Route, Switch, Link, Fragment} from 'react-router-dom'
+import {Route, Switch, Link} from 'react-router-dom'
 import {Module} from './modules/Module'
+import {SignUp} from './User/SignUp'
 import {store} from "./store"
 import {Provider} from "react-redux"
 function App() {
   return (
     <Provider store = {store}>
       <div className="App">
-          <Module/>
+          <Switch>
+            <Route path = "/signup">
+              <SignUp/>
+            </Route>
+            <Route path = "/module">
+              <Module/>
+            </Route>
+
+          </Switch>
       </div>
     </Provider>
   );
