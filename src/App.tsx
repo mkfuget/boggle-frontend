fd5101f0cc968f3eb73c88ebfb6d51eced6cb8cf
mkfuget <michaelfuget@gmail.com>
2021-08-27 15:25:11 -0700

    updated react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {BogglePuzzle} from './modules/boggle/components/BogglePuzzle'
+import {Route, Switch, Link, Fragment} from 'react-router-dom'
 import {Module} from './modules/Module'
 import {store} from "./store"
 import {Provider} from "react-redux"
--- src/user/SignUp.tsx
+++ src/user/SignUp.tsx
@@ -1,22 +1,29 @@
 import React, {useState} from "react"
-
+import UserPool from "../UserPool"
 const SignUp = () => 
 {
-    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const onSubmit = (event: React.SyntheticEvent): void => 
     {
         event.preventDefault();
+        UserPool.signUp(email, password, [], [], (error, data) => {
+            if (error) {
+                console.log(error);
+            }
+            console.log(data);
+        })
+
     };
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <label htmlFor="username">Username</label>
+                <label htmlFor="email">Email</label>
                 <input
-                    value = {username}
-                    onChange = {(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value)}
+                    value = {email}
+                    onChange = {(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.currentTarget.value)}
                 ></input>
                 <label htmlFor="password">Password</label>
                 <input
@@ -29,4 +36,3 @@ const SignUp = () =>
 }
 export default SignUp;
 
-}
