56a34366485da50422768d91192576b7172ef647
mkfuget <michaelfuget@gmail.com>
2021-10-12 14:15:58 -0700

    updated cards css

--- src/lessons/Lesson.tsx
+++ src/lessons/Lesson.tsx
@@ -1,15 +1,21 @@
 import { API } from "aws-amplify";
 import React, {useState} from "react"
+import { useParams } from "react-router-dom";
+import { getLesson } from "../graphql/queries";
 import "./lesson.css";
-
-
+type lessonName = {
+    lessonName: string
+  }
+  
 const Lesson = () => 
 {
+    const { lessonName } = useParams<lessonName>();
+
     const fetchConceptsData = async () => {
         try {
             const conceptsData: any = await (API.graphql({
-              query: getLessons,
-              variables: {id: `${moduleName}Concepts`},
+              query: getLesson,
+              variables: {id: `${lessonName}Concepts`},
               authMode: "AWS_IAM",
             })) 
         } catch (error) {
